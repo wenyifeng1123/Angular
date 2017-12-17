0ffd88931e703dfecb0615f6bcc06c001b1872e3
2010bigbang <wenyifeng1123@gmail.com>
2017-12-17 02:18:39 +0100

    first commtit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,34 @@
 import { Component } from '@angular/core';
+// NgModel 属于一个可选模块FormsModule。我们必须选择使用那个模块
+// Hero 对象
+// 当输入改变时上面小标题也会改变
+export class Hero {
+  id: number;
+  name: string;
+}
 
+// 使用多行模板字符串添加更多 HTML
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>{{title}}</h1>
+  <h2>{{hero.name}} details!!</h2>
+  <div><label>id:</label>{{hero.id}}</div>
+  
+  <div>
+    <label>name:</label> 
+    <input [(ngModel)]="hero.name" placeholder="name">
+  </div>
+  `
 })
-export class AppComponent  { name = 'Angular'; }
+
+
+export class AppComponent  {
+ title= 'Tour of Heros';
+ // hero= 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+ }
+
+
